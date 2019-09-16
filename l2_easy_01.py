04f1960cb8f46e655083231bf2fec9f4226a2b72
Hook <barrabrawa@gmail.com>
2019-09-16 23:36:04 +0300

    Revert "add more homework"
    This reverts commit 7fd50901edfcdb56a7fceb98822ec3fa8f844173.

--- l2_easy_01.py
+++
@@ -1,5 +0,0 @@
-fruits = ["яблоко", "банан", "киви", "арбуз"]
-x = 0
-
-for x in range (len(fruits)):
-        print(f"{x + 1} : {fruits[x]:>}")
--- l2_easy_02.py
+++
@@ -1,4 +0,0 @@
-first_list = [1,2,3,4,5]
-second_list = [4,5,8,9,10]
-
-print(list(set(first_list) - set(second_list)))
